cc2402672a7a10f79b6681abbc6c150791217b53
Piotr Hołubowicz <piotrhol@man.poznan.pl>
2012-06-04 12:01:54 +0200

    Improved constructor of anonId

--- src/main/java/org/purl/wf4ever/rosrs/client/common/AnonId.java
+++ src/main/java/org/purl/wf4ever/rosrs/client/common/AnonId.java
@@ -13,6 +13,17 @@ import java.io.Serializable;
  */
 public class AnonId extends com.hp.hpl.jena.rdf.model.AnonId implements Serializable {
 
+    /**
+     * Constructor.
+     * 
+     * @param id
+     *            Jena anonId
+     */
+    public AnonId(com.hp.hpl.jena.rdf.model.AnonId id) {
+        super(id.getLabelString());
+    }
+
+
     /** id. */
     private static final long serialVersionUID = 5382206987065355069L;
 
