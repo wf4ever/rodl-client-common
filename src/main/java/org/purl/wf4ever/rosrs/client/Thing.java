706fe5e9f30d28a70291a27f62419b6b27de4bfe
Piotr Hołubowicz <piotrhol@man.poznan.pl>
2013-02-07 18:18:35 +0100

    Calculate RO snapshots/lives/archives based on roevo info.

--- src/main/java/org/purl/wf4ever/rosrs/client/Thing.java
+++ src/main/java/org/purl/wf4ever/rosrs/client/Thing.java
@@ -155,4 +155,36 @@ public class Thing implements Serializable {
         creators.add(new Creator(ums, creator));
     }
 
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((uri == null) ? 0 : uri.hashCode());
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Thing other = (Thing) obj;
+        if (uri == null) {
+            if (other.uri != null) {
+                return false;
+            }
+        } else if (!uri.equals(other.uri)) {
+            return false;
+        }
+        return true;
+    }
+
 }
