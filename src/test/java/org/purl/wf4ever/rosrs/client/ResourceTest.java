911243ad5cfc025abf346bad5eec663afd2a160e
Piotr Hołubowicz <piotrhol@man.poznan.pl>
2013-04-26 16:37:31 +0200

    One resource test works offline.

--- src/test/java/org/purl/wf4ever/rosrs/client/ResourceTest.java
+++ src/test/java/org/purl/wf4ever/rosrs/client/ResourceTest.java
@@ -1,18 +1,35 @@
 package org.purl.wf4ever.rosrs.client;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.delete;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpStatus;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 import org.purl.wf4ever.rosrs.client.exception.ROSRSException;
+
+import pl.psnc.dl.wf4ever.vocabulary.ORE;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
 /**
  * Test {@link Resource} methods.
@@ -22,6 +39,10 @@ import org.purl.wf4ever.rosrs.client.exception.ROSRSException;
  */
 public class ResourceTest extends BaseTest {
 
+    /** A test HTTP mock server. */
+    @Rule
+    public static final WireMockRule WIREMOCK_RULE = new WireMockRule(8089); // No-args constructor defaults to port 8080
+
     /** RO that will be mapped to local resources. */
     private static final URI RO_PREFIX = URI.create("http://example.org/ro1/");
 
@@ -30,6 +51,15 @@ public class ResourceTest extends BaseTest {
 
     /** Proxy of the resource that will be mapped to local resources. */
     private static final URI PROXY_URI = URI.create("http://example.org/ro1/proxies/1");
+
+    /** Some RO available by HTTP. */
+    private static final URI MOCK_RO = URI.create("http://localhost:8089/ro1/");
+
+    /** Some resource available by HTTP. */
+    private static final URI MOCK_RESOURCE = URI.create("http://localhost:8089/res1.txt");
+
+    /** Some resource available by HTTP. */
+    private static final URI MOCK_RESOURCE_PROXY = URI.create("http://localhost:8089/resproxy");
 
     /** A loaded RO. */
     private static ResearchObject ro1;
@@ -48,10 +78,17 @@ public class ResourceTest extends BaseTest {
     public static final void setUpBeforeClass()
             throws Exception {
         BaseTest.setUpBeforeClass();
+        rosrs = new ROSRService(URI.create("http://localhost:8089/"), TOKEN);
         ro1 = new ResearchObject(RO_PREFIX, rosrs);
         ro1.load();
         res1 = new Resource(ro1, RES_URI, PROXY_URI, URI.create("http://test1.myopenid.com"), new DateTime(2011, 12,
                 02, 15, 02, 10, DateTimeZone.UTC));
+    }
+
+
+    @Before
+    public void setUp()
+            throws Exception {
     }
 
 
@@ -77,23 +114,26 @@ public class ResourceTest extends BaseTest {
     @Test
     public final void testCreateResearchObjectStringInputStreamString()
             throws ROSRSException, IOException {
-        ResearchObject ro;
-        try {
-            ro = ResearchObject.create(rosrs, "JavaClientTest");
-        } catch (ROSRSException e) {
-            if (e.getStatus() == HttpStatus.SC_CONFLICT) {
-                ro = new ResearchObject(rosrs.getRosrsURI().resolve("JavaClientTest/"), rosrs);
-                ro.delete();
-                ro = ResearchObject.create(rosrs, "JavaClientTest");
-            } else {
-                throw e;
-            }
-        }
-        try (InputStream in = getClass().getClassLoader().getResourceAsStream("ro1/res1.txt")) {
+        // this is what the mock HTTP server will return
+        InputStream response = getClass().getClassLoader().getResourceAsStream("resources/response.rdf");
+        stubFor(post(urlEqualTo("/")).withHeader("Accept", equalTo("application/rdf+xml")).willReturn(
+            aResponse().withStatus(201).withHeader("Content-Type", "application/rdf+xml")
+                    .withHeader("Location", MOCK_RO.toString())));
+        stubFor(delete(urlEqualTo("/ro1/")).willReturn(aResponse().withStatus(204)));
+        stubFor(post(urlEqualTo("/ro1/")).willReturn(
+            aResponse().withStatus(201).withHeader("Content-Type", "text/plain")
+                    .withHeader("Location", MOCK_RESOURCE_PROXY.toString())
+                    .withHeader("Link", "<" + MOCK_RESOURCE + ">; rel=\"" + ORE.proxyFor.toString() + "\"")
+                    .withBody(IOUtils.toByteArray(response))));
+        stubFor(delete(urlEqualTo("/res1.txt")).willReturn(aResponse().withStatus(204)));
+
+        ResearchObject ro = ResearchObject.create(rosrs, "ro1");
+        try (InputStream in = getClass().getClassLoader().getResourceAsStream("resources/res1.txt")) {
             Resource res = Resource.create(ro, "res1.txt", in, "text/plain");
             Assert.assertNotNull(res);
             res.delete();
         }
+        verify(postRequestedFor(urlMatching("/ro1/")).withHeader("Content-Type", equalTo("text/plain")));
         ro.delete();
     }
 
