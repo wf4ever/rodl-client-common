bc295587d9cf1d419cb13df8f249c9fc3be07e9b
Piotr Hołubowicz <piotrhol@man.poznan.pl>
2013-05-02 14:44:10 +0200

    checking job status uses mockups.

--- src/test/java/org/purl/wf4ever/rosrs/client/evo/BaseTest.java
+++ src/test/java/org/purl/wf4ever/rosrs/client/evo/BaseTest.java
@@ -1,15 +1,24 @@
 package org.purl.wf4ever.rosrs.client.evo;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.delete;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.After;
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.purl.wf4ever.rosrs.client.ROSRService;
 import org.purl.wf4ever.rosrs.client.ResearchObject;
-import org.purl.wf4ever.rosrs.client.exception.ROSRSException;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
 
 /**
  * A test class for the RO evo service.
@@ -37,19 +46,14 @@ public class BaseTest {
     /** The Live RO. */
     protected ResearchObject ro;
 
+    /** Some RO available by HTTP. */
+    protected static final URI MOCK_RO = URI.create("http://localhost:8089/ro1/");
 
-    /**
-     * Prepare an ROEVO service.
-     * 
-     * @throws Exception
-     *             example RO has incorrect data
-     */
-    @BeforeClass
-    public static void setUpBeforeClass()
-            throws Exception {
-        rosrs = new ROSRService(RODL_URI.resolve("ROs/"), TOKEN);
-        roevo = new ROEVOService(RODL_URI, TOKEN);
-    }
+    /** Some RO available by HTTP. */
+    protected static final URI MOCK_SNAPSHOT = URI.create("http://localhost:8089/ro1-copy/");
+
+    /** Some RO available by HTTP. */
+    protected static final URI MOCK_JOB_STATUS = URI.create("http://localhost:8089/jobs/1");
 
 
     /**
@@ -61,25 +65,66 @@ public class BaseTest {
     @Before
     public void setUp()
             throws Exception {
-        ro = new ResearchObject(rosrs.getRosrsURI().resolve("ROEVOServiceTest/"), rosrs);
-        ro.delete();
-        ro = ResearchObject.create(rosrs, "ROEVOServiceTest");
-        rosToDelete.add(ro);
+        WireMock.resetAllScenarios();
+        setUpServiceDesc();
+        setUpRoSnapshot();
+        setUpGetStatus();
+
+        rosrs = new ROSRService(URI.create("http://localhost:8089/foo/"), null);
+        roevo = new ROEVOService(URI.create("http://localhost:8089/"), null);
+        ro = new ResearchObject(MOCK_RO, rosrs);
     }
 
 
     /**
-     * Remove all ROs created in the tests.
+     * Configure WireMock to handle snapshotting the RO.
      * 
-     * @throws ROSRSException
-     *             unexpected response from RODL
+     * @throws IOException
+     *             if the test resources are not available
      */
-    @After
-    public void tearDown()
-            throws ROSRSException {
-        for (ResearchObject roToDelete : rosToDelete) {
-            roToDelete.delete();
-        }
+    protected void setUpServiceDesc()
+            throws IOException {
+        InputStream serviceDesc = getClass().getClassLoader().getResourceAsStream("evo/service-desc.rdf");
+        stubFor(get(urlEqualTo("/")).willReturn(
+            aResponse().withStatus(200).withHeader("Content-Type", "application/rdf+xml")
+                    .withBody(IOUtils.toByteArray(serviceDesc))));
     }
 
+
+    /**
+     * Configure WireMock to handle snapshotting the RO.
+     * 
+     * @throws IOException
+     *             if the test resources are not available
+     */
+    protected void setUpRoSnapshot()
+            throws IOException {
+        InputStream jobRunning = getClass().getClassLoader().getResourceAsStream("evo/status-running.json");
+        stubFor(post(urlEqualTo("/copy/")).willReturn(
+            aResponse().withStatus(201).withHeader("Content-Type", "application/json")
+                    .withHeader("Location", MOCK_JOB_STATUS.toString()).withBody(IOUtils.toByteArray(jobRunning)))
+                .willSetStateTo("Job created"));
+        stubFor(delete(urlEqualTo("/jobs/1")).willReturn(aResponse().withStatus(204)));
+    }
+
+
+    /**
+     * Configure WireMock to handle snapshotting the RO.
+     * 
+     * @throws IOException
+     *             if the test resources are not available
+     */
+    protected void setUpGetStatus()
+            throws IOException {
+        InputStream jobRunning = getClass().getClassLoader().getResourceAsStream("evo/status-running.json");
+        stubFor(get(urlEqualTo("/jobs/1")).willReturn(
+            aResponse().withStatus(200).withHeader("Content-Type", "application/json")
+                    .withBody(IOUtils.toByteArray(jobRunning))).willSetStateTo("Job checked #1"));
+        InputStream jobDone = getClass().getClassLoader().getResourceAsStream("evo/status-done.json");
+        stubFor(get(urlEqualTo("/jobs/1"))
+                .whenScenarioStateIs("Job checked #1")
+                .willReturn(
+                    aResponse().withStatus(200).withHeader("Content-Type", "application/json")
+                            .withBody(IOUtils.toByteArray(jobDone))).willSetStateTo("Job done"));
+    }
 }
--- src/test/java/org/purl/wf4ever/rosrs/client/evo/JobStatusTest.java
+++ src/test/java/org/purl/wf4ever/rosrs/client/evo/JobStatusTest.java
@@ -1,9 +1,11 @@
 package org.purl.wf4ever.rosrs.client.evo;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
-import org.purl.wf4ever.rosrs.client.ResearchObject;
 import org.purl.wf4ever.rosrs.client.evo.JobStatus.State;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
 /**
  * Test of refreshing the job status.
@@ -13,13 +15,17 @@ import org.purl.wf4ever.rosrs.client.evo.JobStatus.State;
  */
 public class JobStatusTest extends BaseTest {
 
+    /** A test HTTP mock server. */
+    @Rule
+    public static final WireMockRule WIREMOCK_RULE = new WireMockRule(8089); // No-args constructor defaults to port 8080
+
+
     /**
      * Test that the job status can be refreshed.
      */
     @Test
     public final void testRefresh() {
-        JobStatus status = ro.snapshot("TestSnapshot");
-        rosToDelete.add(new ResearchObject(ro.getUri().resolve(status.getTarget()), rosrs));
+        JobStatus status = ro.snapshot("ro1-copy");
 
         while (status.getState() == State.RUNNING) {
             try {
